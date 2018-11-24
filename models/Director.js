ef107df1a91272d71d8ab3678a48fa3d14bec482
Mahmut Yiğit <mahmutygt96@hotmail.com>
2018-11-24 21:53:43 +0300

    /api/directors validation

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,24 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 
 const DirectorSchema=new Schema({
-    name:String,
-    surname:String,
-    bio:String,
+    name:{
+        type:String,
+        required:true,
+        maxlength:30,
+        minlength:1
+    },
+    surname:{
+        type:String,
+        required:true,
+        maxlength:20,
+        minlength:1
+    },
+    bio:{
+        type:String,
+        required:true,
+        maxlength:300,
+        minlength:10
+    },
     createdAt:{
         type:Date,
         default:Date.now
--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,35 @@ const movieSchema=new Schema({
     directorID:Schema.Types.ObjectId,
     title:{
         type:String,
-        required:true
+        required:true,
+        maxlength:[15,'{PATH} alanı {MAXLENGTH} karakterden az olmalı'],
+        minlength:[3,'{PATH} alanı {MINLENGTH} karakterden fazla olmalı']
     },
-    category:String,
-    country:String,
-    year:Number,
-    imdbScore:Number,
+    category:{
+        type:String,
+        maxlength:15,
+        minlength:3
+    },
+    category:{
+        type:String,
+        maxlength:15,
+        minlength:3
+    },
+    country:{
+        type:String,
+        maxlength:30,
+        minlength:2
+    },
+    year:{
+        type:Number,
+        max:2018,
+        min:1850
+    },
+    imdbScore:{
+        type:Number,
+        max:10,
+        min:0
+    },
     createdAt:{
         type:Date,
         default:Date.now
