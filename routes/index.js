fcc3395b52e386c361ea0db7c2b43a5dd1444874
Mahmut Yiğit <mahmutygt96@hotmail.com>
2018-11-24 22:52:02 +0300

    /register [GET]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+
+const User=require('../models/User');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+  const {username,password}=req.body;
+  
+  const user=new User({
+    username:username,
+    password:password
+  }); 
+
+  const promise=user.save();
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
 });
 
 module.exports = router;
